aee674294347d51efffd37efe91ae47f3d49d708
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2023-04-17 02:44:37 +0300

    Initial commit

---
+++ WebAPIExample/Model/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIExample.Model
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=Faruk;Integrated Security=true"); 
+        }
+    }
+}
